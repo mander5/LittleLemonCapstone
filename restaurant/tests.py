cb1685a790e59425066809c1e5c18f96e665245f
mander5 <aaron_mander@hotmail.com>
2024-08-12 17:10:03 +0100

    setting test folder

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -4,9 +4,9 @@ from .models import Menu
 # Create your tests here.
 
 
-class MenuTest(TestCase):
-    def test_get_item(self):
-        item = Menu.objects.create(title="IceCream", price=80, inventory=100)
-        itemstr = item.get_item()
+# class MenuTest(TestCase):
+#     def test_get_item(self):
+#         item = Menu.objects.create(title="IceCream", price=80, inventory=100)
+#         itemstr = item.get_item()
 
-        self.assertEqual(itemstr, "IceCream : 80")
+#         self.assertEqual(itemstr, "IceCream : 80")
