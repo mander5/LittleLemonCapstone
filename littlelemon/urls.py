7b4347143809012727c0362607b7745fbd430090
mander5 <aaron_mander@hotmail.com>
2024-08-12 16:55:08 +0100

    duplicating homepage to project level url

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from restaurant.views import BookingViewSet
+from . import views
 
 router = DefaultRouter()
 router.register(r'tables', BookingViewSet)
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),
